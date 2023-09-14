89ff7567c9bbf311a5b3369d03d6633511f144bb
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-14 20:38:08 +0530

    add events on buttons

---
+++ script.js
@@ -0,0 +1,16 @@
+var istatus = document.querySelector("h5");
+
+var addfriend = document.querySelector("#add");
+
+var removefriend = document.querySelector("#remove");
+
+
+addfriend.addEventListener("click", function() {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+});
+
+removefriend.addEventListener("click", function() {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "red";
+});
